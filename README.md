62ae37ccc758e8c724142a8a30d283d00ab05f12
Canoral <kaya68@outlook.fr>
2023-05-01 20:16:48 +0200

    Ajout d'un readme et une image du résultat final

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-S03-exo-decompOser
+# Jeu du parcours 🏃‍♂️ 🏃‍♀️ 🚶‍♀️
+
+Ce projet est un petit jeu de parcours dans lequel le joueur doit écrire un script pour guider un personnage du point de départ au point d'arrivée. Le joueur a trois commandes à sa disposition : "left" pour tourner à gauche, "right" pour tourner à droite et "move" pour avancer d'une case dans la direction actuelle. Le joueur écrit son script dans une zone de texte, puis appuie sur un bouton pour lancer le script. Le personnage exécute alors les commandes une par une jusqu'à atteindre le point d'arrivée ou sortir de la grille de jeu.
+
+## Fonctionnalités
+
+Grille de jeu aléatoire de 4 lignes sur 6 colonnes
+Point de départ et point d'arrivée aléatoires
+Commandes "left", "right" et "move"
+Affichage du jeu à chaque tour
+Vérification de la réussite ou de l'échec du parcours
+
+## Utilisation
+
+Pour jouer, il suffit de cloner ce dépôt Git et d'ouvrir le fichier index.html dans un navigateur web. Le jeu se lance automatiquement et une grille de jeu est affichée à l'écran avec le point de départ et le point d'arrivée. Le joueur doit écrire un script pour guider le personnage à travers la grille en utilisant les commandes "left", "right" et "move". Une fois le script écrit, le joueur clique sur le bouton "Lancer le script" pour commencer le parcours. Le jeu affiche alors la grille à chaque tour et le personnage exécute les commandes du script une par une. Si le joueur réussit à amener le personnage au point d'arrivée, une alerte de victoire s'affiche. Sinon, une alerte d'échec est affichée.
+
+## Technologies utilisées
+
+Ce projet a été développé en JavaScript, HTML et CSS, sans l'utilisation de bibliothèques ou de frameworks externes. Le code utilise des fonctions de manipulation du DOM pour afficher la grille de jeu et les alertes de victoire ou d'échec.
+
+![resultat](resultat.jpg)
--- js/app.js
+++ js/app.js
@@ -3,17 +3,17 @@ var app = {
   nbCols: 6,
   start: {
     row: 1,
-    col: 1
+    col: 1,
   },
   end: {
     row: 6,
-    col: 6
+    col: 6,
   },
   current: null,
-  direction: 'right', // top, right, bottom, left
+  direction: "right", // top, right, bottom, left
   delay: 500,
-  init: function() {
-    console.log('init');
+  init: function () {
+    console.log("init");
 
     app.randomStart();
     app.randomEnd();
@@ -21,37 +21,39 @@ var app = {
     app.initGame();
     app.drawBoard();
 
-    document.getElementById('launchScript').addEventListener('click', app.handleLaunchScriptButton);
+    document
+      .getElementById("launchScript")
+      .addEventListener("click", app.handleLaunchScriptButton);
   },
-  randomStart: function() {
-    app.start.row = Math.floor(Math.random() * Math.floor(app.nbRows))+1;
-    app.start.col = Math.floor(Math.random() * Math.floor(app.nbCols))+1;
+  randomStart: function () {
+    app.start.row = Math.floor(Math.random() * Math.floor(app.nbRows)) + 1;
+    app.start.col = Math.floor(Math.random() * Math.floor(app.nbCols)) + 1;
   },
-  randomEnd: function() {
-    app.end.row = Math.floor(Math.random() * Math.floor(app.nbRows))+1;
-    app.end.col = Math.floor(Math.random() * Math.floor(app.nbCols))+1;
+  randomEnd: function () {
+    app.end.row = Math.floor(Math.random() * Math.floor(app.nbRows)) + 1;
+    app.end.col = Math.floor(Math.random() * Math.floor(app.nbCols)) + 1;
   },
-  initGame: function() {
+  initGame: function () {
     app.current = {
       row: app.start.row,
-      col: app.start.col
+      col: app.start.col,
     };
-    app.direction = 'right';
+    app.direction = "right";
   },
-  handleLaunchScriptButton: function() {
+  handleLaunchScriptButton: function () {
     app.initGame();
 
     // Strating displaying the board
     app.drawBoard();
 
     // get code
-    var codeLines = document.getElementById('userCode').value.split("\n");
+    var codeLines = document.getElementById("userCode").value.split("\n");
 
-    window.setTimeout(function() {
+    window.setTimeout(function () {
       app.codeLineLoop(codeLines, 0);
     }, app.delay);
   },
-  codeLineLoop: function(codeLines, index) {
+  codeLineLoop: function (codeLines, index) {
     var currentLine = codeLines[index];
 
     var continueReading = app.interpretLine(currentLine);
@@ -64,27 +66,27 @@ var app = {
       // if still a line to interpret
       if (index < codeLines.length) {
         // Recall same method
-        window.setTimeout(function() {
+        window.setTimeout(function () {
           app.codeLineLoop(codeLines, index);
         }, app.delay);
       } else {
-        window.setTimeout(function() {
+        window.setTimeout(function () {
           app.checkSuccess();
         }, app.delay);
       }
     } else {
-      alert('Game over...');
+      alert("Game over...");
     }
   },
-  interpretLine: function(line) {
-    if (line == 'turn left') {
+  interpretLine: function (line) {
+    if (line == "left") {
       app.turnLeft();
-    } else if (line == 'turn right') {
+    } else if (line == "right") {
       app.turnRight();
-    } else if (line == 'move forward') {
+    } else if (line == "move") {
       var moveOk = app.moveForward();
       if (!moveOk) {
-        alert('BRAIN ERROR ! out of bounds ...');
+        alert("BRAIN ERROR ! out of bounds ...");
         return false;
       }
     } else {
@@ -94,44 +96,44 @@ var app = {
 
     return true;
   },
-  checkSuccess: function() {
+  checkSuccess: function () {
     // if coordinates are ok
     if (app.current.row == app.end.row && app.current.col == app.end.col) {
-      alert('You WIINNN !!! congratulations !');
+      alert("You WIINNN !!! congratulations !");
     } else {
-      alert('You dramatically failed !');
+      alert("You dramatically failed !");
     }
   },
-  drawBoard: function() {
-    var boardElement = document.getElementById('board');
-    boardElement.innerHTML = '';
+  drawBoard: function () {
+    var boardElement = document.getElementById("board");
+    boardElement.innerHTML = "";
 
     var currentRow;
     var currentCell;
     for (var i = 1; i <= app.nbRows; i++) {
-      currentRow = document.createElement('div');
-      currentRow.classList.add('cellRow');
-      currentRow.setAttribute('id', 'row' + i);
+      currentRow = document.createElement("div");
+      currentRow.classList.add("cellRow");
+      currentRow.setAttribute("id", "row" + i);
 
       for (var j = 1; j <= app.nbCols; j++) {
-        currentCell = document.createElement('div');
-        currentCell.classList.add('cell');
-        currentCell.classList.add('cell' + j);
+        currentCell = document.createElement("div");
+        currentCell.classList.add("cell");
+        currentCell.classList.add("cell" + j);
 
         // if start
         if (i == app.start.row && j == app.start.col) {
-          currentCell.classList.add('cellStart');
+          currentCell.classList.add("cellStart");
         }
         // if end
         if (i == app.end.row && j == app.end.col) {
-          currentCell.classList.add('cellEnd');
+          currentCell.classList.add("cellEnd");
         }
         // if current
         if (i == app.current.row && j == app.current.col) {
           // current
-          currentCell.classList.add('cellCurrent');
+          currentCell.classList.add("cellCurrent");
           // direction
-          currentCell.classList.add('cellCurrent-' + app.direction);
+          currentCell.classList.add("cellCurrent-" + app.direction);
         }
 
         currentRow.appendChild(currentCell);
@@ -140,77 +142,77 @@ var app = {
       boardElement.appendChild(currentRow);
     }
   },
-  moveForward: function() {
+  moveForward: function () {
     switch (app.direction) {
-      case 'left':
+      case "left":
         app.current.col--;
         break;
-      case 'right':
+      case "right":
         app.current.col++;
         break;
-      case 'top':
+      case "top":
         app.current.row--;
         break;
-      case 'bottom':
+      case "bottom":
         app.current.row++;
         break;
       default:
-        console.log('direction unknown : ' + app.direction);
+        console.log("direction unknown : " + app.direction);
     }
 
     // if out of bounds => return false
     if (app.current.row < 1) {
       app.current.row = 1;
-      console.log('out 1');
+      console.log("out 1");
       return false;
     } else if (app.current.row > app.nbRows) {
       app.current.row = app.nbRows;
-      console.log('out 2');
+      console.log("out 2");
       return false;
     } else if (app.current.col < 1) {
       app.current.col = 1;
-      console.log('out 3');
+      console.log("out 3");
       return false;
     } else if (app.current.col > app.nbCols) {
       app.current.col = app.nbCols;
-      console.log('out 4');
+      console.log("out 4");
       return false;
     }
 
     return true;
   },
-  turnLeft: function() {
+  turnLeft: function () {
     switch (app.direction) {
-      case 'left':
-        app.direction = 'bottom';
+      case "left":
+        app.direction = "bottom";
         break;
-      case 'right':
-        app.direction = 'top';
+      case "right":
+        app.direction = "top";
         break;
-      case 'top':
-        app.direction = 'left';
+      case "top":
+        app.direction = "left";
         break;
-      case 'bottom':
-        app.direction = 'right';
+      case "bottom":
+        app.direction = "right";
         break;
     }
   },
-  turnRight: function() {
+  turnRight: function () {
     switch (app.direction) {
-      case 'left':
-        app.direction = 'top';
+      case "left":
+        app.direction = "top";
         break;
-      case 'right':
-        app.direction = 'bottom';
+      case "right":
+        app.direction = "bottom";
         break;
-      case 'top':
-        app.direction = 'right';
+      case "top":
+        app.direction = "right";
         break;
-      case 'bottom':
-        app.direction = 'left';
+      case "bottom":
+        app.direction = "left";
         break;
     }
-  }
+  },
 };
 
-document.addEventListener('DOMContentLoaded', app.init);
+document.addEventListener("DOMContentLoaded", app.init);
